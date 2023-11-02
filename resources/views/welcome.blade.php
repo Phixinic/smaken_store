43ddbdb3d5b559843c3ca93b5f8b2e1bb7028f17
NalendraUtama <nalendrautama@gmail.com>
2023-11-02 08:51:39 +0700

    bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -390,7 +390,7 @@
     </style>
 </head>
 
-<body class="antialiased">
+<body class="antialiased bg-secondary">
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
