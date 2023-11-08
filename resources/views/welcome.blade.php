bc89fc00c57d3f674cd6d18b1f08585eab858503
rosea <121949840+roRosaa@users.noreply.github.com>
2023-11-08 15:54:10 +0700

    card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,29 @@
     </ul>
 </div>
 {{-- Bar Fitur --}}
+{{-- content --}}
+<div class="container">
+    <div class="row m-2">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
