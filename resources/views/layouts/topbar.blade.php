dc9098152869377564e602972a579911391660b6
NalendraUtama <nalendrautama@gmail.com>
2023-11-08 14:22:20 +0700

    halaman utama

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -8,39 +8,30 @@
 
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg bg-light shadow fixed-top">
         <div class="container-fluid d-flex">
           <a class="navbar-brand" href="#">Smaken Store</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          
-          {{-- <div class="d-flex justify-content-end order-2" >
-            <div>
-                <button class="btn btn-outline-primary rounded-0 py-2 px-4">Sign in</button>
-            </div>
-            <div>
-                <button class="btn btn-primary rounded-0 py-2 px-4">Log in</button>
-            </div>
-          </div> --}}
+          </button> --}}
 
 
-          <div class="collapse navbar-collapse justify-content-center " id="navbarNav">
+          <div class="d-flex justify-content-center " id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item">
+              <li class="nav-item px-3">
                 <a class="nav-link active" href="#">Home</a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item px-3">
                 <a class="nav-link" href="#">About</a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item px-3">
                 <a class="nav-link" href="#">FAQ</a>
               </li>
             </ul>
           </div>
 
           <div class="d-flex justify-content-end" >
-            <div>
+            <div class="mx-2">
                 <button class="btn btn-outline-primary rounded-0 py-2 px-4">Sign in</button>
             </div>
             <div>
@@ -51,8 +42,13 @@
         </div>
       </nav>
 {{-- CONTENT --}}
-      @yield('content')
+
+  @yield('content')
+
 {{-- CONTENT --}}
+      <footer class=" align-self-end bg-primary p-5">
+
+      </footer>
   </body>
 </html>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.topbar')
 
 @section('content')
-
+<div class="container-fluid mt-5 pt-5">
+    {{-- HERO --}}
+    <div class="container px-5">
+        <div class="hero" id="hero">
+            <div><img src="https://source.unsplash.com/random/1000x300" alt=""></div>
+    
+        </div>
+    </div>
+    {{-- HERO --}}
 <h1>Welcome</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore ea veniam, pariatur ex praesentium quam consequuntur iure tenetur eligendi necessitatibus minima consequatur sapiente vel neque! Unde sequi accusantium eligendi maxime velit nemo aperiam modi ut assumenda ratione rem, est enim explicabo quaerat ullam ea nam placeat animi quibusdam itaque distinctio! Recusandae cupiditate quis odit amet optio quisquam quibusdam, fugit aspernatur reprehenderit quae nam. Inventore veniam iusto laudantium nemo odit harum libero! Natus, dolorem. Deleniti minus nemo voluptatibus ab numquam iste aut fuga, optio quidem eius assumenda cumque necessitatibus velit eos eaque amet adipisci earum voluptate tempora quos laboriosam sit.</p>
+</div>
+
+
 @endsection
