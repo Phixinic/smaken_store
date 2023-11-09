124385cc989b91ff33c58e3c6413e9c07e7e52d6
NalendraUtama <nalendrautama@gmail.com>
2023-11-09 07:59:48 +0700

    halaman utama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,24 @@
 @section('content')
 <div class="container-fluid mt-5 pt-5">
     {{-- HERO --}}
-    <div class="container px-5">
+    <div class="container px-5 mb-4">
         <div class="hero" id="hero">
-            <div><img src="https://source.unsplash.com/random/1000x300" alt=""></div>
+            <div><img src="https://source.unsplash.com/random/1000x300" alt="" class="img-fluid"></div>
     
         </div>
     </div>
     {{-- HERO --}}
-<h1>Welcome</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore ea veniam, pariatur ex praesentium quam consequuntur iure tenetur eligendi necessitatibus minima consequatur sapiente vel neque! Unde sequi accusantium eligendi maxime velit nemo aperiam modi ut assumenda ratione rem, est enim explicabo quaerat ullam ea nam placeat animi quibusdam itaque distinctio! Recusandae cupiditate quis odit amet optio quisquam quibusdam, fugit aspernatur reprehenderit quae nam. Inventore veniam iusto laudantium nemo odit harum libero! Natus, dolorem. Deleniti minus nemo voluptatibus ab numquam iste aut fuga, optio quidem eius assumenda cumque necessitatibus velit eos eaque amet adipisci earum voluptate tempora quos laboriosam sit.</p>
 </div>
+{{-- Bar Fitur --}}
+<div class="bg-primary my-3">
+    <ul class="d-flex justify-content-evenly flex-wrap">
+        <a href="" class="btn p-4">TopUp Game</a>
+        <a href="" class="btn p-4">TopUp Pulsa</a>
+        <a href="" class="btn p-4">Merch</a>
+        <a href="" class="btn p-4">Produk</a>
+    </ul>
+</div>
+{{-- Bar Fitur --}}
 
 
 @endsection
