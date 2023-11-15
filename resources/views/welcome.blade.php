3ba5a4ef4b98933b5a84845c8bf3337851b951a0
Farid Heka <faridhkrio926@gmail.com>
2023-11-15 14:12:23 +0700

    Update welcome.blade.php
    menambah anjir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
     {{-- HERO --}}
 </div>
 {{-- Bar Fitur --}}
-<div class="bg-primary my-3">
+<div class="bg-primary my-2">
     <ul class="d-flex justify-content-evenly flex-wrap">
         <a href="" class="btn p-4 text-light fw-bolder"><i class="bi bi-gem"></i>  TopUp Game</a>
         <a href="" class="btn p-4 text-light fw-bolder"><i class="bi bi-stars"></i>  TopUp Pulsa</a>
@@ -24,8 +24,116 @@
 {{-- Bar Fitur --}}
 {{-- content --}}
 <div class="container">
+    <div class="text-center">
+        <h1>All Product</h1>
+        <div class="container-fluid justify-content-center w-50">
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+    </div>
     <div class="row m-2">
-        <div class="card" style="width: 18rem;">
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
+            <div class="card-body">
+                <p class="card-category text-secondary">Merch</p>
+                <p class="card-title fs-4">Nama Produk</p>
+              <p class="card-text">Some quick example text.</p>
+              <p class="card-price fw-bold fs-5">Rp.9000</p>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 18rem;">
             <img src="{{ ('assets/img/test1.jpg') }}" class="card-img-top mt-2" alt="...">
             <div class="card-body">
                 <p class="card-category text-secondary">Merch</p>
