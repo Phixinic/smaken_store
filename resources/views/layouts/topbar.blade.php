89ebff0d60825d0ce68c35f63814cf06ac053894
NalendraUtama <nalendrautama@gmail.com>
2023-11-15 13:46:06 +0700

    Top bar responsive pokok e apik ws

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -3,14 +3,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ASD</title>
+    <title>Smaken Store</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/topbar.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg bg-light shadow fixed-top">
+    {{-- <nav class="navbar navbar-expand-lg bg-light shadow fixed-top">
         <div class="container-fluid d-flex justify-content-between">
           <div>
             <img src="{{ asset('assets/img/smkn1lmj.png') }}" class="ms-5 logo" alt="">
@@ -46,8 +47,42 @@
           </div>
 
         </div>
+      </nav> --}}
+<!-- Navbar start -->
+<nav class="navbar navbar-expand-lg bg-light shadow fixed-top">
+<div class="container-fluid ">
+  <div class="container">
+      <nav class="navbar navbar-light navbar-expand-lg ">
+        <img src="{{ asset('assets/img/smkn1lmj.png') }}" class="logo" alt="">
+          <a href="" class="navbar-brand">
+              <h3 class="text-primary fw-bold mb-0">Smaken<span class="text-dark">Store</span> </h3>
+          </a>
+          <button class="navbar-toggler py-2 px-3 rounded-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="navbar-nav mx-auto">
+                <li class="nav-item px-3">
+                  <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item px-3">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item px-3">
+                  <a class="nav-link" href="#">FAQ</a>
+                </li>
+              </div>
+                <div class="d-flex">
+                  <a href="#" class="btn btn-outline-primary py-2 px-4 rounded-0">Log In</a>
+                  <a href="#" class="btn btn-primary py-2 px-4 rounded-0 mx-2">Sign Up</a>
+                </div>
+          </div>
       </nav>
-{{-- CONTENT --}}
+  </div>
+</div>
+</nav>
+<!-- Navbar End -->
+      {{-- CONTENT --}}
 
   @yield('content')
 
@@ -56,6 +91,6 @@
 
       </footer>
   </body>
+  
 </html>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
